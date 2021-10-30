fedd403a46335bb9527cfc4e4f5f36f6fa6070b7
unknown <marcus.larsen.swe@gmail.com>
2021-10-30 23:26:41 +0200

    yeah känner mig slö

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from "react";
+
 
 function App() {
+
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+    Name: <input type="text" name="username" onChange={e => {setUser(e.target.value)}}/>
+    Password: <input type="text" name="username" onChange={e => {setPassword(e.target.value)}}/>
+    <button onClick={e =>alert(user)}>Login</button><button onClick={e =>alert(password)}>Register </button>
+      </div>
     </div>
   );
 }
