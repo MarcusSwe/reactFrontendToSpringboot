3c5ae281e3a94250a60a09eaf452f1808c11de8f
unknown <marcus.larsen.swe@gmail.com>
2021-10-31 14:49:41 +0100

    super tired going super slow

--- src/App.js
+++ src/App.js
@@ -6,17 +6,45 @@ function App() {
 
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState("not logged in");
+
+ function addUser() {
+   async function fetchAdd(){
+     const resp = await fetch('http://localhost:8080/user/register', {
+       method: 'PUT',
+       headers: { 'Content-Type': 'application/json' },
+       body: JSON.stringify({
+         username: user,
+         password: password
+       }) 
+     })
+     switch(resp.status) {
+       case 409:
+         alert("user already exist")
+         break;
+       case 500:
+         alert("something went wrong")
+         break;
+      default:
+        alert("user created")         
+     }
+   }
+   fetchAdd();
+ }
 
   return (
     <div>
       <div>
     Name: <input type="text" name="username" onChange={e => {setUser(e.target.value)}}/>
     Password: <input type="text" name="username" onChange={e => {setPassword(e.target.value)}}/>
-    <button onClick={e =>alert(user)}>Login</button><button onClick={e =>alert(password)}>Register </button>
-      </div>
-      lets see if git is retarded
+    <button onClick={e =>alert(user)}>Login</button><button onClick={e =>{addUser()}}>Register </button>
+    <button>show own favorites</button><button>log off</button>{loggedIn}
+      </div>      
     </div>
   );
 }
 
 export default App;
+
+
+// <button onClick={e =>alert(user)}>
