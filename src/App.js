3b7ac94bcfa3f79b2ff88d55d93af82fb7b107f5
unknown <marcus.larsen.swe@gmail.com>
2021-10-31 18:15:12 +0100

    yeah soon done break now though

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 
 function App() {
@@ -8,6 +8,10 @@ function App() {
   const [password, setPassword] = useState("");
   const [loggedIn, setLoggedIn] = useState("not logged in");
   const [token, setToken] = useState("");
+  const [product, setProduct] = useState("");
+  const [productDesc, setProductDesc] = useState("");
+  const [price, setPrice] = useState(0);
+  const [products, setProducts] = useState([]);
 
  function addUser() {
    async function fetchAdd(){
@@ -20,7 +24,6 @@ function App() {
        }) 
      })
      const stringrespons = await resp.text();
-
      switch(resp.status) {
        case 409:         
          alert(stringrespons)
@@ -42,13 +45,12 @@ function loginUser() {
       headers: ({
        'username': user,
        'password': password
-    })
+      })
     })
     const tokenstring = await resp.text();
     if(resp.status===406){
       alert("something went wrong");
-    } else {
-    
+    } else {    
     setToken(tokenstring);
     setLoggedIn("logged in as: " + user);
     }
@@ -61,7 +63,7 @@ function logoffUser(){
     if(token.length>1){
     const resp = await fetch('http://localhost:8080/user/logout', {
       method: 'POST',
-      headers: { 'token': token}
+      headers: { 'token': token }
      }) 
      setToken("");
      setLoggedIn("not logged in");
@@ -71,6 +73,49 @@ function logoffUser(){
   fetchLogOff();
 }
 
+function createProduct() {
+  async function fetchCreate(){
+    const resp = await fetch('http://localhost:8080/product/create', {
+      method: 'PUT',
+      headers: { 'token': token,
+      'Content-Type': 'application/json'
+     },
+      body: JSON.stringify({
+        name: product,
+        description: productDesc,
+        price: price
+      })
+    })
+    const stringrespons = await resp.text();
+    switch(resp.status) {
+      case 409:         
+        alert(stringrespons)
+        break;
+      case 500:
+        alert(stringrespons)
+        break;
+     default:
+       alert("product created")         
+    }
+  }
+  fetchCreate();
+  getProducts();
+}
+
+function getProducts() {
+  fetch('http://localhost:8080/product/all', {
+    method: 'GET',
+    headers: { 'token': token }
+   })
+    .then(resp => resp.json())
+    .then(data => setProducts(data));
+}
+
+useEffect(() => {
+   if(token.length>1){
+    getProducts();
+   } else setProducts([]);    
+}, [token]);
 
   return (
     <div>
@@ -79,7 +124,46 @@ function logoffUser(){
     Password: <input type="text" name="username" onChange={e => {setPassword(e.target.value)}}/>
     <button onClick={e =>{loginUser()}}>Login</button><button onClick={e =>{addUser()}}>Register </button>
     <button>show own favorites</button><button onClick={e =>{logoffUser()}}>log off</button>{loggedIn}
-      </div>      
+      </div>     
+      <div>
+        Product: <input type="text" name="product" onChange={e => {setProduct(e.target.value)}}/>
+        Product Description: <input type="text" name="productDesc" onChange={e => {setProductDesc(e.target.value)}}/>
+        Product Price: <input type="number" name="price" onChange={e => {setPrice(e.target.value)}}/>
+        <button onClick={e =>{createProduct()}}>Create</button>
+      </div> 
+      <div>
+           <ul>
+                {products.map((product, index) => {
+                    return (<li key={index}>
+                        {product.name} | {product.price} | {product.description}
+                        <button onClick={() => {
+                            fetch('http://localhost:8080/product/add-favorite', {
+                                method: 'PUT',
+                                headers: { 'token': token,
+                                'Content-Type': 'application/json'
+                               },
+                                body: JSON.stringify({
+                                  productName: product                                  
+                                })                                
+                            }).then(resp => {
+                              
+                              switch(resp.status) {
+                                case 404:         
+                                  alert(resp.text())
+                                  break;
+                                case 500:
+                                  alert(resp.texg())
+                                  break;
+                               default:
+                                 alert("product created")         
+                              }                                
+                            });
+                        }}>Add favorite</button>
+                    </li>);
+                })}
+            </ul>
+      </div>
+
     </div>
   );
 }
